6b28cd1147fd9dba62459639186bc9ef174da6ce
Samuel Kow Paintsil <smashbros54@gmail.com>
2021-09-02 22:16:56 +0000

    radix sort

--- main.cpp
+++ main.cpp
@@ -67,9 +67,11 @@ void insertionSort(int arr[], int arrSize){
 }
 
 void shellShort(int arr[], int arrSize){
+    //int pass = 0;
     for(int gap = arrSize/2; gap > 0; gap/=2){
         for(int j = gap; j < arrSize; j++){
             for(int i = j - gap; i >= 0; i = i-gap){
+      //          pass++;
                 if(arr[i+gap] > arr[i])
                     break;
                 int tmp = arr[i+gap];
@@ -89,17 +91,60 @@ void shellShort(int arr[], int arrSize){
         std::cout<<arr[i]<<",";
     }
     std::cout<<"]"<<std::endl;
+    //std::cout<<"Shell Passes: "<<pass<<std::endl;
 
 }
 
+int getMax(int arr[], int arrSize){
+    int max = arr[0];
+    for(int i = 0; i < arrSize; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+void countSort(int arr[], int arrSize, int pos){
+    int countArr[10] = {0};
+    int outputArr[arrSize];
+    for(int i = 0; i<arrSize; i++)
+        countArr[(arr[i]/pos) % 10]++;// Set count array
+    for(int i = 0; i < 10; i++)
+        countArr[i] += countArr[i-1]; // Update count array
+    for(int i = arrSize-1; i >= 0; i--) {
+        outputArr[countArr[(arr[i] / pos) % 10] - 1] = arr[i]; // Fill output array
+        countArr[(arr[i]/pos) % 10]--; // Reduce value in count array of the found number
+    }
+
+    for(int i = 0 ; i < arrSize; i++)
+        arr[i] = outputArr[i];
+}
+
+void radixSort(int arr[], int arrSize){
+    int maxNum = getMax(arr, arrSize);
+    for(int pos = 1; maxNum / pos > 0; pos*=10){
+        countSort(arr,arrSize,pos);
+    }
+
+    // Print
+    std::cout<<"Radix Sorted Array: [";
+    for(int i = 0; i < arrSize; i++){
+        std::cout<<arr[i]<<",";
+    }
+    std::cout<<"]"<<std::endl;
+}
+
 int main() {
-    int numArray[] = {2,4,19,20,1,5,3,50,55,12};
+    //int numArray[] = {2,4,19,20,1,5,3,50,55,12};
+    int numArray[] = {432,8,530,90,88,231,11,45,677,199};
     int arraySize = sizeof(numArray)/sizeof(numArray[0]);
     std::cout << "Array Size: " <<arraySize<< std::endl;
     bubbleSort(numArray, arraySize);
     selectionSort(numArray, arraySize);
     insertionSort(numArray, arraySize);
     shellShort(numArray, arraySize);
+    radixSort(numArray, arraySize);
 
 
 
